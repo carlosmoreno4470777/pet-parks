170b68f452f3964b5a93c1673439e454ad96ac47
carlosmoreno4470777 <carlosmoreno.4470777@gmail.com>
2024-03-21 00:42:23 -0500

    testing commit changes

--- pet-parks/src/main/java/pet/park/controller/ParkController.java
+++ pet-parks/src/main/java/pet/park/controller/ParkController.java
@@ -1,14 +1,23 @@
 package pet.park.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import lombok.extern.slf4j.Slf4j;
 import pet.park.controller.model.ContributorData;
+import pet.park.controller.model.PetParkData;
 import pet.park.service.ParkService;
 
 @RestController
@@ -25,5 +34,60 @@ public class ParkController {
         return parkService.saveContributor(contributorData);
         //return parkService.saveContributor(contributorData);
 	}
+    
+    @PutMapping("/contributor/{contributorId}")
+    public ContributorData updateContributor(@PathVariable Long contributorId,
+        @RequestBody ContributorData contributorData) {
+      contributorData.setContributorId(contributorId);
+      log.info("Updating contributor {}", contributorData);
+      return parkService.saveContributor(contributorData);
+    }
+    
+    
+    @GetMapping("/contributor")
+    public List<ContributorData> retrieveAllContributors() {
+      log.info("Retrieving all contributors.");
+      return parkService.retrieveAllContributors();
+    }
+    
+    @GetMapping("/contributor/{contributorId}")
+    public ContributorData retrieveContributorById(
+        @PathVariable Long contributorId) {
+      log.info("Retrieving contributor with ID={}", contributorId);
+      return parkService.retrieveContributorById(contributorId);
+    }
+    
+    @DeleteMapping("/contributor")
+    public void deleteAllContributors(
+        @PathVariable Long contributorId) {
+      log.info("Attempting to delte all contributors");
+      throw new UnsupportedOperationException("Deleting all contributors is not allowed ");
+    }
+    
+    //We are accepting a contributorId for the table in the URL
+    @DeleteMapping("/contributor/{contributorId}")
+    public Map<String, String> deleteContributorById(
+        @PathVariable Long contributorId) {
+      log.info("Deleting contributor with ID={}", contributorId);
+
+      //we are deleting on the service
+      parkService.deleteContributorById(contributorId);
+
+      //if it succesfull it will get to this point
+      return Map.of("message",
+          "Contributor with ID=" + contributorId + " deleted successfully.");
+    }
+    
+    @PostMapping("/contributor/{contributorId}/park")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public PetParkData insertPetPark(@PathVariable Long contributorId,
+        @RequestBody PetParkData petParkData) {
+
+      log.info("Creating park {} for contributor with ID={}", petParkData,
+          contributorId);
+      return parkService.savePetPark(contributorId, petParkData);
+    }
+    
+    
     
 }// end of class ParkController
--- pet-parks/src/main/java/pet/park/service/parkService.java
+++ pet-parks/src/main/java/pet/park/service/parkService.java
@@ -1,62 +1,178 @@
 package pet.park.service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import pet.park.controller.model.ContributorData;
+import pet.park.controller.model.PetParkData;
+import pet.park.dao.AmenityDao;
 import pet.park.dao.ContributorDao;
+import pet.park.dao.PetParkDao;
+import pet.park.entity.Amenity;
 import pet.park.entity.Contributor;
-
+import pet.park.entity.PetPark;
 
 @Service
 public class ParkService {
-	
+
+	  @Autowired
+	  private AmenityDao amenityDao;
+
+	  @Autowired
+	  private PetParkDao petParkDao;
+
+	  /**
+	   * The @Autowired annotation instructs Spring to inject an object of the
+	   * required type into the instance variable.
+	   */
 	  @Autowired
 	  private ContributorDao contributorDao;
 
-	@Transactional (readOnly = false)
+	@Transactional(readOnly = false)
 	public ContributorData saveContributor(ContributorData contributorData) {
-	    Long contributorId = contributorData.getContributorId();
-	    Contributor contributor = findOrCreateContributor(contributorId);
-	    
-	    setFieldsInContributor(contributor, contributorData);
-	    
-	    return new ContributorData(contributorDao.save(contributor));
- 
+		Long contributorId = contributorData.getContributorId();
+		// Contributor contributor = findOrCreateContributor(contributorId);
+		Contributor contributor = findOrCreateContributor(contributorId, contributorData.getContributorEmail());
 
-		//return null;
+		setFieldsInContributor(contributor, contributorData);
+
+		return new ContributorData(contributorDao.save(contributor));
+
+		// return null;
 	}
 
-	  private void setFieldsInContributor(Contributor contributor,
-		      ContributorData contributorData) {
-		    contributor.setContributorEmail(contributorData.getContributorEmail());
-		    contributor.setContributorName(contributorData.getContributorName());
-		  }
+	private void setFieldsInContributor(Contributor contributor, ContributorData contributorData) {
+		contributor.setContributorEmail(contributorData.getContributorEmail());
+		contributor.setContributorName(contributorData.getContributorName());
+	}
 
-	private Contributor findOrCreateContributor(Long contributorId) {
+	/*
+	 * private Contributor findOrCreateContributor(Long contributorId) { Contributor
+	 * contributor;
+	 * 
+	 * if (Objects.isNull(contributorId)) { contributor = new Contributor(); } else
+	 * { contributor = findContributorById(contributorId); }
+	 * 
+	 * return contributor; }
+	 */
+
+	private Contributor findOrCreateContributor(Long contributorId, String contributorEmail) {
 		Contributor contributor;
-	    
-	    if (Objects.isNull(contributorId)) {
-	    	contributor = new Contributor();
-	    }
-	    else {
-	    	contributor = findContributorById(contributorId);
-	    }
-	    
-	    return contributor;
 
+		if (Objects.isNull(contributorId)) {
+			Optional<Contributor> opContrib = contributorDao.findByContributorEmail(contributorEmail);
+
+			if (opContrib.isPresent()) {
+				throw new DuplicateKeyException(" Contributor with email " + contributorEmail + " already exist ");
+			} else {
+
+			}
+
+			contributor = new Contributor();
+		} else {
+			contributor = findContributorById(contributorId);
+		}
+
+		return contributor;
 	}
 
 	private Contributor findContributorById(Long contributorId) {
 
-		return contributorDao.findById(contributorId)
-				.orElseThrow(() -> new NoSuchElementException(
-						"Contributor with ID=" + contributorId + " was  not found."));
+		return contributorDao.findById(contributorId).orElseThrow(
+				() -> new NoSuchElementException("Contributor with ID=" + contributorId + " was  not found."));
 	}
 
+	@Transactional(readOnly = true)
+	public List<ContributorData> retrieveAllContributors() {
+		/*
+		 * Uncomment the code below to retrieve all contributors using traditional
+		 * (non-functional) coding techniques. An enhanced for loop is used to convert
+		 * from the list of Contributor to the list of ContributorData required by the
+		 * method return type.
+		 */
+		// List<Contributor> contributors = contributorDao.findAll();
+		// List<ContributorData> response = new LinkedList<>();
+		//
+		// for(Contributor contributor : contributors) {
+		// response.add(new ContributorData(contributor));
+		// }
+		//
+		// return response;
+
+		// @formatter:off
+		    return contributorDao.findAll() // Retrieves a List of Contributor
+		        .stream()                   // Converts to a Stream of Contributor
+		        .map(ContributorData::new)  // Converts to a Stream of ContributorData
+		        .toList();                  // Converts to a List of ContributorData
+		    // @formatter:on
+	}
+
+	@Transactional(readOnly = true)
+	public ContributorData retrieveContributorById(Long contributorId) {
+		Contributor contributor = findContributorById(contributorId);
+		return new ContributorData(contributor);
+	}
+
+	@Transactional(readOnly = false)
+	public void deleteContributorById(Long contributorId) {
+		Contributor contributor = findContributorById(contributorId);
+		contributorDao.delete(contributor);
+	}
+
+	@Transactional(readOnly = false)
+	public PetParkData savePetPark(Long contributorId, PetParkData petParkData) {
+		// find contributor by finding contributor ID
+		Contributor contributor = findContributorById(contributorId);
+
+		Set<Amenity> amenities = amenityDao.findAllByAmenityIn(petParkData.getAmenities());
+
+		// passing pet park ID that might be null
+		PetPark petPark = findOrCreatePetPark(petParkData.getPetParkId());
+		setPetParkFields(petPark, petParkData);
+
+		petPark.setContributor(contributor);
+		contributor.getPetParks().add(petPark);
+
+		for (Amenity amenity : amenities) {
+			amenity.getPetParks().add(petPark);
+			petPark.getAmenities().add(amenity);
+		}
+
+		PetPark dbPetPark = petParkDao.save(petPark);
+		return new PetParkData(dbPetPark);
+
+	}
+
+	private void setPetParkFields(PetPark petPark, PetParkData petParkData) {
+	    petPark.setCountry(petParkData.getCountry());
+	    petPark.setDirections(petParkData.getDirections());
+	    petPark.setGeoLocation(petParkData.getGeoLocation());
+	    petPark.setParkName(petParkData.getParkName());
+	    petPark.setPetParkId(petParkData.getPetParkId());
+	    petPark.setStateOrProvince(petParkData.getStateOrProvince());		
+	}
+
+	private PetPark findOrCreatePetPark(Long petParkId) {
+		PetPark petPark;
+
+		if (Objects.isNull(petParkId)) {
+			petPark = new PetPark();
+		} else {
+			petPark = findPetParkById(petParkId);
+		}
+
+		return petPark;
+	}
+
+	private PetPark findPetParkById(Long petParkId) {
+		return petParkDao.findById(petParkId)
+				.orElseThrow(() -> new NoSuchElementException("Pet park with ID=" + petParkId + " does not exist."));
+	}
 
 }
